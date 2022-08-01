72f8bc6f9af22d431dd6502b5b47b1560e680118
rubentxogg <rubentxogg@hotmail.com>
2022-08-01 20:46:26 +0200

    [ADD] Empty search img

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  top15BooksByMonthAndYearApiBaseUrl: 'https://hapi-books.p.rapidapi.com/month',
+  searchBookByNameApiBaseUrl: 'https://hapi-books.p.rapidapi.com/search',
+  awardedBooksOfAYearUrl: "https://hapi-books.p.rapidapi.com/top",
+  weeklyPopularBooksByGenreUrl: "https://hapi-books.p.rapidapi.com/week",
+  XRapidAPIHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHeaderValue: 'hapi-books.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8deaa31b8dmsh5b87e23e302820fp172d2ajsn4050dd4c5bf7'
 };
