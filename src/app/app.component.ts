23fa6b3f5a1e1bddf3c7d4f1f753057d66cf493c
rubentxogg <rubentxogg@hotmail.com>
2022-08-07 12:50:35 +0200

    [MOD] build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.router.navigate(['home'], { relativeTo: this.route });
+  }
 }
